19afdc8de4a022cfa37b702b39119db61d670620
Emilio Rizzo <metano@2669709.no-reply.drupal.org>
2016-08-26 21:09:46 -0300

    cleanup code

--- js/social_sharrre.js
+++ js/social_sharrre.js
@@ -1,17 +1,23 @@
 (function ($) {
+  'use strict';
   Drupal.behaviors.sharrre = {
     attach: function (context, settings) {
-     
-     var setts = jQuery.parseJSON( Drupal.settings.sharrre.setts );
-     var pref = Drupal.settings.sharrre.pref;
-     
-      for(var  s in setts){
-        $( '.'+pref+s.toLowerCase() ).each(function(i, obj){
-         var sobj = setts[s];
-         sobj.srv = s;
-         sobj.click = function(api, options){api.simulateClick();api.openPopup(this.srv);};
-         $( this ).sharrre(sobj);
-        });
+
+      var setts = jQuery.parseJSON(Drupal.settings.sharrre.setts);
+      var pref = Drupal.settings.sharrre.pref;
+
+      function selCall(i, obj) {
+        var sobj = setts[s];
+        sobj.srv = s;
+        sobj.click = function (api, options) { api.simulateClick(); api.openPopup(this.srv); };
+        $(this).sharrre(sobj);
+      }
+
+      for (s in setts) {
+        if (setts.hasOwnProperty(s)) {
+          var selector = '.' + pref + s.toLowerCase();
+          $(selector).each(selCall(i, obj));
+        }
       }
     }
   };
